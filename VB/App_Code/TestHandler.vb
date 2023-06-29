2c054afdb6afad451553fac64fdee019c6229c55
DevExpressExampleBot <fakeEmail>
2023-06-29 10:37:58 +0400

    Source auto update [skip ci]

---
+++ VB/App_Code/TestHandler.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Web
+Imports System.Web.SessionState
+
+Public Class TestHandler
+	Implements IHttpHandler, IRequiresSessionState
+
+	Private Function GetProgress(ByVal session As HttpSessionState) As String
+		If session Is Nothing OrElse Not (TypeOf session("_Operation") Is Operation) Then
+			Return "NaN"
+		End If
+		Dim progress = (TryCast(session("_Operation"), Operation)).Progress
+		Return progress.ToString()
+	End Function
+
+	Public ReadOnly Property IsReusable() As Boolean
+		Get
+			Return False
+		End Get
+	End Property
+
+	Public Sub ProcessRequest(ByVal context As HttpContext)
+		Dim response = GetProgress(context.Session)
+		context.Response.CacheControl = "No-cache"
+		context.Response.Write(response)
+	End Sub
+End Class
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Private ReadOnly Property Operation() As Operation
+		Get
+			If Session("_Operation") Is Nothing Then
+				Session("_Operation") = New Operation()
+			End If
+			Return TryCast(Session("_Operation"), Operation)
+		End Get
+	End Property
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+
+	Protected Sub cbak_Callback(ByVal source As Object, ByVal e As DevExpress.Web.CallbackEventArgs)
+		Operation.AsyncStart(5000)
+	End Sub
+End Class
